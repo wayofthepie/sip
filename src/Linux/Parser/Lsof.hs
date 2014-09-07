6efe3971d31a839a3cdcac5d68108ac11ca0faf8
Stephen O'Brien <wayofthepie@gmail.com>
2014-09-07 21:19:59 +0100

    Move impl specific parsers to their own module

---
+++ src/Linux/Parser/Lsof.hs
@@ -0,0 +1,44 @@
+module Linux.Parser.Lsof where
+
+import Control.Applicative hiding (many,(<|>))
+import Text.Parsec
+import Text.Parsec.String
+
+import Linux.Parser.Internal.Lsof
+
+data PIDInfo = PIDInfo {
+        _pid    :: Int,
+        _gid    :: Int,
+        _ppid   :: Int,
+        _cmdname:: String,
+        _uid    :: Int
+    } deriving (Eq, Show)
+
+
+data FileInfo = FileInfo {
+        _fd         :: String,
+        _inode      :: Int,
+        _fileName   :: String
+    } deriving (Eq, Show)
+
+
+lsofp :: Parser [ ( PIDInfo, [FileInfo] ) ]
+lsofp = manyTill ( (,) <$> pidInfop <*> many fileSetp ) eof
+
+
+fileSetp :: Parser FileInfo
+fileSetp = FileInfo
+    <$> fdp <* newline 
+    <*> option (-1) ( inodep <* newline ) 
+    <*> fileNamep <* newline
+  
+
+pidInfop :: Parser PIDInfo
+pidInfop = PIDInfo
+        <$> pidp <* newline
+        <*> procGroupIdp <* newline
+        <*> ppidp <* newline
+        <*> procCmdNamep <* newline
+        <*> procUserIdp <* newline
+
+
