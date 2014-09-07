79f8974cd33081b1256e99d4cfcf475e12f93913
Stephen O'Brien <wayofthepie@gmail.com>
2014-09-07 22:00:46 +0100

    Begin work on graph representation of lsof output

--- src/Linux/Graph/SystemInfo.hs
+++ src/Linux/Graph/SystemInfo.hs
@@ -1,4 +1,17 @@
 
 module Linux.Graph.SystemInfo where
 
+import Data.Graph.Inductive as GI
+
 import Linux.Parser.Lsof
+
+
+data NodeLabel = FDType String | File String deriving (Eq, Show)
+
+
+
+
+{-g :: Gr [NodeLabel] [PIDInfo] 
+g = GI.mkGraph [(1, fdTypeLbl "cwd"),(2, fileLbl "/"),(3, fileLbl "/")] [(1,2,"s")] -}
+
+
