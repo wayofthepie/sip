a080c985a17f3aa67e35313e5d9f12e8b68e1fe6
Stephen O'Brien <wayofthepie@gmail.com>
2014-09-07 20:23:49 +0100

    Cleanup lsof parser

--- src/Linux/Parser/Internal/Lsof.hs
+++ src/Linux/Parser/Internal/Lsof.hs
@@ -1,6 +1,6 @@
 
 -- * Example usage
---     liftM (parse lsofp "") $ readProcess "lsof" ["-F","cgpRu"] ""
+--      liftM (parse lsofp "") $ readProcess "lsof" ["-n", "-F","cfgpRuni"] ""
 
 module Linux.Parser.Internal.Lsof where
 
@@ -10,48 +10,6 @@ import System.Process
 import Text.Parsec
 import Text.Parsec.String
 
-{-
-data LsofCST = LsofCST {
-        _accessMode         :: String,  -- a    file access mode
-        _procCmdName        :: String,  -- c    process command name
-        --_fStructShareCount  :: String,  -- C    file structure share count
-        _fDecCharCode       :: String,  -- d    file's device character code
-        _fdevNum            :: String,  -- D    file's major/minor device number (0x<hexadecimal>)
-        _fDescriptor        :: String,  -- f    file descriptor
-        _fStructAddr        :: String,  -- F    file structure address (0x<hexadecimal>)
-        _fFlags             :: String,  -- G    file flaGs (0x<hexadecimal>; names if +fg follows)
-        _procGroupId        :: Int,     -- g    process group ID
-        _fInodeNum          :: String,  -- i    file's inode number
-        _taskId             :: String,  -- K    tasK ID
-        _linkCount          :: String,  -- k    link count
-        _fLockStatus        :: String,  -- l    file's lock status
-        _procLoginName      :: String,  -- L    process login name
-        _m                  :: String,  -- m    marker between repeated output
-        _fNameCommentIAddr  :: String,  -- n    file name, comment, Internet address
-        _nodeId             :: String,  -- N    node identifier (ox<hexadecimal>)
-        _fOffset            :: String,  -- o    file's offset (decimal)
-        _pid                :: Int,     -- p    process ID (always selected)
-        _protocolName       :: String,  -- P    protocol name
-        --_rawDevNum          :: String,  -- r    raw device number (0x<hexadecimal>)
-        _ppid               :: String,  -- R    parent process ID
-        _fSize              :: String,  -- s    file's size (decimal)
-        _fStreamId          :: String,  -- S    file's stream identification
-        _fType              :: String,  -- t    file's type
-        _tcpTpiInfo         :: String,  -- T    TCP/TPI information, identified by prefixes
-        _procUserId         :: String   -- u    process user ID
-    } deriving (Eq, Show)
--}
-
-{- data NodeLabel = FDType String | File String deriving (Eq, Show)
-data PIDInfo = PIDInfo Int deriving (Eq, Show)
-
-fdTypeLbl :: String -> NodeLabel
-fdTypeLbl n = FDType n
-
-
-fileLbl :: String -> NodeLabel
-fileLbl f = File f
--}
 
 data PIDInfo = PIDInfo {
         _pid    :: Int,
@@ -60,6 +18,7 @@ data PIDInfo = PIDInfo {
         _cmdname:: String,
         _uid    :: Int
     } deriving (Eq, Show)
+
 
 data FileInfo = FileInfo {
         _fd         :: String,
