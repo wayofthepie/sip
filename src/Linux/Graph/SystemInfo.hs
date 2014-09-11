1b188a811212866dde6ffb0f62ef2cc11e69ca81
Stephen O'Brien <wayofthepie@gmail.com>
2014-09-11 20:47:31 +0100

    Update docs for SystemInfo module

--- src/Linux/Graph/SystemInfo.hs
+++ src/Linux/Graph/SystemInfo.hs
@@ -1,5 +1,8 @@
 
-module Linux.Graph.SystemInfo where
+module Linux.Graph.SystemInfo (
+        NodeLabel,
+        lsofToGraph
+    ) where
 
 import Control.Monad
 import Data.Graph.Inductive 
@@ -11,14 +14,19 @@ import Linux.Parser.Lsof
 data NodeLabel = PID PIDInfo | File FileInfo deriving (Eq, Ord, Show)
 
 
+-- | lsofToGraph : the graph of the list l (pidinfo and related fileinfo) constructed by 
+--      mapping pidinfo nodes to fileinfo nodes with the filedescriptor type (e.g. mem, cwd) 
+--      as edges
 lsofToGraph :: [(PIDInfo, [FileInfo])] -> Gr NodeLabel String
 lsofToGraph = lsofToGraph' empty 
 
-
+-- | lsofToGraph' g l : auxiliary function for lsofToGraph 
 lsofToGraph' :: Gr NodeLabel String -> [(PIDInfo, [FileInfo])]-> Gr NodeLabel String
 lsofToGraph' g []   = g
 lsofToGraph' g xs   = run_ empty $ mapM genNodeMapM xs
 
+
+-- | genNodeMapM l : nodemap generate from the pidinfo/fileinfo list l
 genNodeMapM :: DynGraph g =>  (PIDInfo, [FileInfo]) -> NodeMapM NodeLabel String g [()]
 genNodeMapM (pidInfo, fileInfos) = 
     insMapNodeM (PID pidInfo) >> 
