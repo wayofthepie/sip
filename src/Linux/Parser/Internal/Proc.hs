74685b760760c6a82c01c1fb3e667f3e9ad750d0
Stephen O'Brien <wayofthepie@gmail.com>
2014-09-21 09:16:19 +0100

    Add parser for /proc/[pid]/numa_maps
    Also removes trailing whitespace

--- src/Linux/Parser/Internal/Proc.hs
+++ src/Linux/Parser/Internal/Proc.hs
@@ -24,8 +24,8 @@ module Linux.Parser.Internal.Proc (
         iop,
         mapsp,
         environp,
-        procstatp
-
+        procstatp,
+        numamapsp
     ) where
 
 import Control.Applicative hiding (empty)
@@ -42,17 +42,17 @@ import Prelude hiding (takeWhile)
 -- | Data type for \/proc\/[pid]\/maps
 data MappedMemory = MM {
 
-        _address :: (ByteString, ByteString),        
+        _address :: (ByteString, ByteString),
         -- ^ Memory address in the form (start-address, end-address)
-        
-        _perms   :: ByteString,              
-        _offset  :: ByteString,       
+
+        _perms   :: ByteString,
+        _offset  :: ByteString,
 
         _dev     :: (ByteString, ByteString),
-        -- ^ Device number in the form (Major num, Minor num)        
+        -- ^ Device number in the form (Major num, Minor num)
 
         _inode   :: ByteString,
-        _pathname:: Maybe ByteString        
+        _pathname:: Maybe ByteString
     } deriving (Eq, Show)
 
 
@@ -77,16 +77,16 @@ mmPathname  = _pathname
 -- | Parser for __\/proc\/meminfo__.
 --
 -- @
---  openFile "\/proc\/meminfo" ReadMode >>= 
---      \h -> handleToInputStream h >>= 
+--  openFile "\/proc\/meminfo" ReadMode >>=
+--      \h -> handleToInputStream h >>=
 --          \is -> parseFromStream  meminfop is
 --
--- [("MemTotal","4052076",Just "kB"),("MemFree","3450628",Just "kB"), ...] 
+-- [("MemTotal","4052076",Just "kB"),("MemFree","3450628",Just "kB"), ...]
 -- @
 meminfop :: Parser [(ByteString, ByteString, Maybe ByteString)]
-meminfop = manyTill ((,,) 
-    <$> idp 
-    <*> ( skipspacep *> intp <* skipspacep ) 
+meminfop = manyTill ((,,)
+    <$> idp
+    <*> ( skipspacep *> intp <* skipspacep )
     <*> unitp <* skipMany space) endOfInput
 
 
@@ -95,31 +95,31 @@ meminfop = manyTill ((,,)
 -- | Parser for __\/proc\/[pid]\/stat__.
 --
 -- @
---  openFile "\/proc\/1\/stat" ReadMode >>= 
---      \h -> handleToInputStream h >>= 
+--  openFile "\/proc\/1\/stat" ReadMode >>=
+--      \h -> handleToInputStream h >>=
 --          \is -> parseFromStream procstatp is
 --
 --  ["1","(systemd)",\"S\","0","1", ...]
 -- @
 procstatp :: Parser [ByteString]
-procstatp = manyTill psval endOfInput 
+procstatp = manyTill psval endOfInput
 
 
 psval ::  Parser ByteString
 psval = ( takeWhile ( inClass "a-zA-z0-9()-" ) <* space )
-          
+
 
 
 ------------------------------------------------------------------------------
--- | Parser for __\/proc\/loadavg__. Only parses the 1, 5 and 15 minute load 
--- average, discards the fourth and fifth fields (kernel scheduling entities 
+-- | Parser for __\/proc\/loadavg__. Only parses the 1, 5 and 15 minute load
+-- average, discards the fourth and fifth fields (kernel scheduling entities
 -- and latest PID assigned).
 --
 -- @
---  openFile "\/proc\/loadavg" ReadMode >>= 
---      \h -> handleToInputStream h >>= 
+--  openFile "\/proc\/loadavg" ReadMode >>=
+--      \h -> handleToInputStream h >>=
 --          \is -> parseFromStream loadavgp is
---  
+--
 --  ("0.00","0.01","0.05")
 -- @
 loadavgp :: Parser (ByteString, ByteString, ByteString)
@@ -128,30 +128,30 @@ loadavgp = (,,) <$> doublep <*> doublep <*> doublep
 
 
 ------------------------------------------------------------------------------
--- | Parser for __\/proc\/uptime__. The first field is the uptime of the system 
+-- | Parser for __\/proc\/uptime__. The first field is the uptime of the system
 -- (seconds), the second is the amount of time spent in the idle process
 -- (seconds).
--- 
--- @   
---  openFile "\/proc\/uptime" ReadMode >>= 
---      \h -> handleToInputStream h >>= 
+--
+-- @
+--  openFile "\/proc\/uptime" ReadMode >>=
+--      \h -> handleToInputStream h >>=
 --          \is -> parseFromStream uptimep is
 --
 --  ("13048.12","78085.17")
 -- @
 uptimep :: Parser (ByteString, ByteString)
-uptimep = (,) <$> doublep <*> doublep 
+uptimep = (,) <$> doublep <*> doublep
 
 
 
 ------------------------------------------------------------------------------
 -- | Parser for __\/proc\/[pid]\/comm__.
--- 
+--
 -- @
---  openFile "\/proc\/1\/comm" ReadMode >>= 
---      \h -> handleToInputStream h >>= 
+--  openFile "\/proc\/1\/comm" ReadMode >>=
+--      \h -> handleToInputStream h >>=
 --          \is -> parseFromStream (commp) is
---  
+--
 --  "systemd"
 -- @
 commp :: Parser ByteString
@@ -163,26 +163,26 @@ commp = takeWhile ( inClass "a-zA-Z0-9:/" )
 -- | Parser for __\/proc\/[pid]\/io__.
 --
 -- @
---  openFile "\/proc\/1\/io" ReadMode >>= 
---      \h -> handleToInputStream h >>= 
+--  openFile "\/proc\/1\/io" ReadMode >>=
+--      \h -> handleToInputStream h >>=
 --          \is -> parseFromStream (iop) is
---  
+--
 --  [("rchar","12983399"),("wchar","14957379"), ...]
 --
 -- @
 iop :: Parser [(ByteString, ByteString)]
 iop = manyTill ((,) <$> idp <*> ( skipspacep *> intp <* skipMany space )  ) endOfInput
-    
+
 
 
 -------------------------------------------------------------------------------
 -- | Parser for __\/proc\/[pid]\/maps__.
 --
 -- @
---  openFile "\/proc\/1\/maps" ReadMode >>= 
---      \h -> handleToInputStream h >>= 
+--  openFile "\/proc\/1\/maps" ReadMode >>=
+--      \h -> handleToInputStream h >>=
 --          \is -> parseFromStream mapsp is
--- 
+--
 --  [MM {_address = ("7f9c51069000","7f9c5107e000"), _perms = "r-xp", ...]
 -- @
 mapsp :: Parser [MappedMemory]
@@ -192,27 +192,27 @@ mapsp = manyTill ( mapsrowp <* endOfLine ) endOfInput
 
 -- | Parse a row of \/proc\/[pid]\/maps
 mapsrowp :: Parser MappedMemory
-mapsrowp = MM 
+mapsrowp = MM
     <$> addressp <* skipspacep
     <*> permp <* skipspacep
     <*> hdp <* skipspacep
-    <*> devicep <* skipspacep 
+    <*> devicep <* skipspacep
     <*> intp <* skipspacep
-    <*> pathnamep 
+    <*> pathnamep
     where
         addressp :: Parser (ByteString, ByteString)
         addressp = (,) <$> ( hdp <* char '-' ) <*> hdp
-        
+
         permp :: Parser ByteString
-        permp = takeWhile $ inClass "-rwxp" 
+        permp = takeWhile $ inClass "-rwxp"
 
         devicep :: Parser (ByteString, ByteString)
         devicep = (,) <$> ( intp <* char ':' ) <*> intp
-    
+
         pathnamep :: Parser (Maybe ByteString)
-        pathnamep = peekChar >>= \c -> case c of  
-                Just '\n'   -> return Nothing 
-                _           -> liftA Just $ 
+        pathnamep = peekChar >>= \c -> case c of
+                Just '\n'   -> return Nothing
+                _           -> liftA Just $
                                 takeWhile ( inClass "a-zA-Z0-9:/.[]-" )
 
 
@@ -221,8 +221,8 @@ mapsrowp = MM
 -- | Parser for __\/proc\/[pid]\/environ__.
 --
 -- @
---  openFile "/proc/373/environ" ReadMode >>= 
---      \h -> handleToInputStream h >>= 
+--  openFile "/proc/373/environ" ReadMode >>=
+--      \h -> handleToInputStream h >>=
 --          \is -> parseFromStream environp is
 --
 --  [("LANG","en_IE.UTF-8"),("PATH","/usr/local/sbin"), ...]
@@ -232,9 +232,31 @@ environp :: Parser [(ByteString, ByteString)]
 environp = sepBy environrowp $ char '\NUL'
 
 environrowp :: Parser (ByteString, ByteString)
-environrowp = (,) <$> 
-    ( ( takeTill $ inClass "=" ) <* char '=' ) 
+environrowp = (,) <$>
+    ( ( takeTill $ inClass "=" ) <* char '=' )
     <*> ( takeTill $ inClass "\NUL" )
+
+
+
+
+-----------------------------------------------------------------------------
+-- | Parser for \/proc\/[pid]\/numa_maps. Generates a ist of 3-tuples :
+-- (start addr of mem range, mem policy for range, extra info on pages in
+-- range).
+--
+-- @
+--  openFile "/proc/1/numa_maps" ReadMode >>=
+--      \h -> handleToInputStream h >>=
+--          \is -> parseFromStream numamapsp is
+--
+-- [("7f9c51069000","default",["file=/usr/lib/libz.so.1.2.8", ...]
+-- @
+numamapsp :: Parser [(ByteString, ByteString, [ByteString])]
+numamapsp = manyTill ( (,,)
+    <$> ( hdp <* skipspacep )
+    <*> ( takeWhile ( inClass "a-zA-Z" ) <* skipspacep )
+    <*> ( sepBy ( takeWhile $ inClass "-a-zA-Z0-9=/." ) $ char ' ' )
+    <*  endOfLine ) endOfInput
 
 
 
@@ -253,8 +275,8 @@ idp = takeWhile ( inClass "a-zA-z0-9()_" ) <* (skipMany $ char ' ') <*  char ':'
 
 -- | Parse and integer
 intp :: Parser ByteString
-intp = takeWhile $ inClass "0-9" 
-    
+intp = takeWhile $ inClass "0-9"
+
 
 -- | Parse kB
 unitp :: Parser (Maybe ByteString)
